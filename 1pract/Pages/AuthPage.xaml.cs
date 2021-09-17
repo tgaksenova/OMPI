2b494b9192cfce25f4a148ccc886a1f3d675f2ac
P-DK <186158@edu.fa.ru>
2021-09-17 17:36:55 +0300

    Попытка

---
+++ 1pract/Pages/AuthPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1pract.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+       private void ButtonRegistration_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBoxLogin.Text) || string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                MessageBox.Show("Введите логин и пароль!");
+                return;
+            }
+            var db = new Entities();
+                var user = db.User
+                    .AsNotracking()
+                    .FirstOrDefault(u => u.Login == TextBoxLogin.Text && u.Password == PasswordBox.Password);
+
+                if (user == null)
+                {
+                    MessageBox.Show("Пользователь с такими данными не найден!");
+                    return;
+
+                }
+
+                MessageBox.Show("Пользователь успешной найден!");
+
+                switch (user.Role)
+                {
+                    case "Заказчик":
+                        NavigationService?.Navigate(new Menu());
+                    break;
+                case "Директор":
+                    NavigationService?.Navigate(new Menu());
+                    break;
+            }
+
+            db.Dispose();
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new Page2());
+        }
+    }
+}
